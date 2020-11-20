4757e5750964624543d7f3f3cf59b56952bbf08f
davian-wright <71860216+davian-wright@users.noreply.github.com>
2020-11-19 21:12:08 -0500

    Modified world.js and world.php

--- world.php
+++ world.php
@@ -11,8 +11,8 @@ $stmt = $conn->query("SELECT * FROM countries");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
+echo '<ul>'
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
-</ul>
+echo'</ul>'
